8bb1cc5f423766455c0350b81ca37036e3e6f837
Free Dev Group Git <freedev6@gmail.com>
2022-04-09 14:19:52 +0000

    update prod files

--- resiapi/User/admin.py
+++ resiapi/User/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from User.models import *
+
+@admin.register(CustomUser)
+class ClientAdmin(admin.ModelAdmin):
+
+    list_display = ('username', 'email')
